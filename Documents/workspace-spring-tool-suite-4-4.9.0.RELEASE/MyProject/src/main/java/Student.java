e7a1cdf958a83d2358f31dd80938085237365615
MANSI <mansisawant4112@gmail.com>
2021-01-28 12:16:47 +0530

    first commit through sts

---
+++ Documents/workspace-spring-tool-suite-4-4.9.0.RELEASE/MyProject/src/main/java/Student.java
@@ -0,0 +1,56 @@
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "student",length = 20,nullable = false)
+	private String name;
+
+	public Student() {
+		super();
+	}
+	
+	@OneToOne(cascade = CascadeType.ALL)    //for reflecting changes in both entities
+	@JoinColumn(name = "addressId")         //to denote foriegn key for the table
+	private Address address;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+}
